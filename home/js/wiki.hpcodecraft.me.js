dc547187a2cd2376b6c093ec91efe2ad34dc5b65
hpcodecraft <thesquidpeople@googlemail.com>
2017-08-20 05:14:58 +0200

    Adds dotjs script for wiki

---
+++ home/js/wiki.hpcodecraft.me.js
@@ -0,0 +1,48 @@
+$(function() {
+  if(location.pathname === '/spiele-keys') {
+
+    var bullet = ':pentagram:';
+    var button = $('<button id="steam-export">Export for Steam</button>');
+
+    button
+      .css({
+        margin: '20px'
+      })
+      .on('click', function() {
+        var list = createList(bullet);
+
+        console.log(list);
+
+        var textarea = $('<textarea id="steam-export-textarea">'+list+'</textarea>');
+        textarea.css({
+          width: '100%',
+          height: '500px',
+          fontFamily: '"Lucida Console", Monaco, monospace',
+          fontSize: '10px'
+        });
+
+        $('.codowiki_west_header').after(textarea);
+        textarea.focus().select();
+      });
+
+    $('#dokuwiki__sitetools').after(button);
+  }
+});
+
+function createList(bullet) {
+  var list = [];
+  var table = $('table.inline').first();
+  table.find('tr').each(function(i, row) {
+    if(i === 0) return;
+
+    var cells = $(row).find('td');
+    var name = cells.first().html();
+    if(name === undefined) return;
+    var key = cells.next().html();
+
+    if($(key).is('a')) name += ' (Humble Bundle gift link)';
+    list.push(bullet + ' ' + name);
+  });
+
+  return list.join('\n');
+}
